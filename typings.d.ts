6e7e86efb5697f8ec15f0b5a339e96fa216bb88e
Simon Schick <SimonSchick@users.noreply.github.com>
2017-05-14 18:03:32 +0200

    Update and typings (#11)
    * add typings, update most deps, fixed some tests not running at all
    
    * run tests for relevant platforms
    
    * add options for twitter

---
+++ typings.d.ts
@@ -0,0 +1,43 @@
+interface ITesters {
+    twitter: {
+        handle (str: string, options?: { at?: boolean }): boolean;
+        profile (str: string): boolean;
+    }
+    player: {
+        name (str: string): boolean;
+        url (str: string): boolean;
+    }
+    facebook: {
+        name (str: string): boolean;
+        profile (str: string): boolean;
+    }
+    youtube: {
+        name (str: string): boolean;
+        url (str: string): boolean;
+    }
+    steam: {
+        name (str: string): boolean;
+        id (str: string): boolean;
+        profileUrl (str: string): boolean;
+        customUrl (str: string): boolean;
+    }
+    instagram: {
+        name (str: string): boolean;
+        url (str: string): boolean;
+    }
+    soundcloud: {
+        name (str: string): boolean;
+        url (str: string): boolean;
+    }
+    spreadshirt: {
+        url (str: string): boolean;
+        name (str: string): boolean;
+        shop (str: string): boolean;
+    }
+    patreon: {
+        name (str: string): boolean;
+        url (str: string): boolean;
+    }
+}
+
+export const is: ITesters;
